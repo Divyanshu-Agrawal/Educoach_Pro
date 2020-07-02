108109c58f181bc100ade32b0b81b3047a723bce
Divyanshu Agrawal <divyanshu.agrawal.96@gmail.com>
2020-07-02 14:23:56 +0530

    Initial Commit

---
+++ app/build/generated/source/buildConfig/demo/debug/com/aaptrix/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aaptrix;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aaptrix.demoinstitute";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "demo";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "v1.0.2";
+  // Fields from product flavor: demo
+  public static final String SCHOOL_ID = "30";
+  public static final String SCHOOL_NAME = "Demo Institute";
+  public static final String SENDER_ID = "EDCACH";
+}
