436d142dd891d5fdb6ea53ba3465bfb430012081
Divyanshu Agrawal <divyanshu.agrawal.96@gmail.com>
2020-07-03 16:29:56 +0530

    Added functionality for hiding more videos option in youtube player.

---
+++ app/build/generated/source/buildConfig/scoremax/debug/com/aaptrix/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aaptrix;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aaptrix.scoremax";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "scoremax";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "1.0.9";
+  // Fields from product flavor: scoremax
+  public static final String SCHOOL_ID = "42";
+  public static final String SCHOOL_NAME = "ScoreMax";
+  public static final String SENDER_ID = "SCOMAX";
+}
