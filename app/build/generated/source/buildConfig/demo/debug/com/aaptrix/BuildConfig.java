147d110b13247ba2613d66882ec59df371944676
Divyanshu Agrawal <divyanshu.agrawal.96@gmail.com>
2020-07-11 22:39:23 +0530

    Config File Changes

---
+++ app/build/generated/source/buildConfig/demo/debug/com/aaptrix/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aaptrix;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aaptrix.demoinstitute";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "demo";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "v1.0.4";
+  // Fields from product flavor: demo
+  public static final String SCHOOL_ID = "30";
+  public static final String SCHOOL_NAME = "Demo Institute";
+  public static final String SENDER_ID = "EDCACH";
+  public static final String TNC = "https://dashboard.educoachapp.com/policies/terms.html";
+}
